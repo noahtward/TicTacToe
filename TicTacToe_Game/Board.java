df1a289f24a268176b59f7b033c16da5a5afb0db
Noah Ward <noahtward08@gmail.com>
2025-04-12 15:28:39 -0500

    Update

--- TicTacToe_Game/Board.java
+++ TicTacToe_Game/Board.java
@@ -6,6 +6,16 @@
 
 public class Board {
     private char[][] board;
+    private int[][] winCombos = {
+        {0, 1, 2},
+        {3, 4, 5},
+        {6, 7, 8},
+        {0, 3, 6},
+        {1, 4, 7},
+        {2, 5, 8},
+        {0, 4, 8},
+        {2, 4, 6}
+    };
 
     //Constructor, initializes board
     public Board() {
@@ -63,4 +73,13 @@ public class Board {
             throw new InvalidMoveException("Board space already taken");
         }
     }
+
+    // public boolean checkWin(int move, char symbol) {
+    //     int[] moveCoordinate = boardNumToCoordinate(move);
+    //     if (move == 4) {
+    //         for (int i = 0; i < 6; i++) {
+    //             for ()
+    //         }
+    //     }
+    // }
 }
